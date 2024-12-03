f35be3a0b2f5b47515fede9f6b6f310ee236ac87
MrMadera <90923803+MrMadera@users.noreply.github.com>
2024-12-03 20:47:35 +0000

    fuck i forgor the sprite :sob:

--- flixel/extended/ExtendedSprite.hx
+++ flixel/extended/ExtendedSprite.hx
@@ -2,21 +2,44 @@ package flixel.extended;
 
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.graphics.frames.FlxAtlasFrames;
 
 class ExtendedSprite extends FlxSprite
 {
-    public function new(x:Float = 0, y:Float = 0, ?simpleGraphic:Null<FlxGraphicAsset>)
+    public var graphicPath:String;
+    public var isAnimated:Bool;
+
+    public function new(x:Float = 0, y:Float = 0, ?simpleGraphic:Null<FlxGraphicAsset>, _isAnimated:Bool = false)
     {
+        isAnimated = _isAnimated;
+
         if(simpleGraphic == null)
         {
             simpleGraphic = 'assets/images/no_image.png';
         }
+        graphicPath = simpleGraphic;
+
+        trace('Path: $graphicPath');
 
         super(x, y, simpleGraphic);
+
+        if(isAnimated)
+        {
+            frames = FlxAtlasFrames.fromSparrow(simpleGraphic, StringTools.replace(graphicPath, '.png', '.xml'));
+        }
     }
 
     public function addSparrowAnimation(name:String, anim:String, framerate:Int, loop:Bool)
     {
+        if(!isAnimated) return;
+
         animation.addByPrefix(name, anim, framerate, loop);
+    }
+
+    public function playAnim(name:String)
+    {
+        if(!isAnimated) return;
+
+        animation.play(name);
     }
 }
