44ee6a4564dfcbd4efbec6b564fd68229b0ea7fb
MrMadera <90923803+MrMadera@users.noreply.github.com>
2024-12-03 17:01:11 +0000

    if the graphic is null, an image with a text 'null' will be loaded

--- flixel/extended/ExtendedSprite.hx
+++ flixel/extended/ExtendedSprite.hx
@@ -1,11 +1,17 @@
 package flixel.extended;
 
 import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
 
 class ExtendedSprite extends FlxSprite
 {
-    public function new(x:Float = 0, y:Float = 0, ?simpleGraphic:Null<FlxGraphicAssets>)
+    public function new(x:Float = 0, y:Float = 0, ?simpleGraphic:Null<FlxGraphicAsset>)
     {
+        if(simpleGraphic == null)
+        {
+            simpleGraphic = 'assets/images/no_image.png';
+        }
+        
         super(x, y, simpleGraphic);
     }
 }
