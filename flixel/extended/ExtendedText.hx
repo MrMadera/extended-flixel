52d41dd3d27470dfce5cbe084bdbf935f0cab250
MrMadera <90923803+MrMadera@users.noreply.github.com>
2024-12-20 14:10:54 +0000

    i forgor to push the real extended text file lol

---
+++ flixel/extended/ExtendedText.hx
@@ -0,0 +1,16 @@
+package flixel.extended;
+
+import flixel.text.FlxText;
+
+class ExtendedText extends FlxText
+{
+    public function new(x:Float, y:Float, width:Int, text:String, size:Int)
+    {
+        super(x, y, width, text, size);
+    }
+
+    public function setBold(isbold:Bool)
+    {
+        bold = isbold;
+    }
+}
