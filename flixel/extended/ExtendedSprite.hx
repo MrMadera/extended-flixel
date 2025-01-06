f92d51f6de9e3cd25b537aaa769d4bb46144599e
MrMadera <ismaelglhs@gmail.com>
2025-01-06 23:59:54 +0000

    descriptions to ExtendedSprite.hx

--- flixel/extended/ExtendedSprite.hx
+++ flixel/extended/ExtendedSprite.hx
@@ -11,11 +11,27 @@ class ExtendedSprite extends FlxSprite
 {
     // TODO: Add descriptions to everything
 
+    /**
+     * The graphic path is where the graphics is located in the `assets` folder
+    **/
     public var graphicPath:String;
+
+    /**
+     * If `true`, the class will automaticlly set animation settings
+    **/
     public var isAnimated:Bool;
 
+    /**
+     * The elapsed time since the class is instanced
+    **/
     public var elapsedTime:Float;
 
+    /**
+     *@param x x position
+     *@param y y position
+     *@param simpleGraphic the graphic of the sprite. it can be a `Dynamic` or a `String`
+     *@param _isAnimated if the sprite is animated or not
+    **/
     public function new(x:Float = 0, y:Float = 0, ?simpleGraphic:Null<FlxGraphicAsset>, _isAnimated:Bool = false)
     {
         isAnimated = _isAnimated;
@@ -45,6 +61,9 @@ class ExtendedSprite extends FlxSprite
         elapsedTime += elapsed;
     }
 
+    /**
+     * adds an sparrow animation to the sprite. if the sprite is not animated, this function won't cause effect
+    **/
     public function addSparrowAnimation(name:String, anim:String, framerate:Int, loop:Bool)
     {
         if(!isAnimated) return;
@@ -52,6 +71,9 @@ class ExtendedSprite extends FlxSprite
         animation.addByPrefix(name, anim, framerate, loop);
     }
 
+    /**
+     * plays an animation. if the sprite is not animated, this function won't cause effect
+    **/
     public function playAnim(name:String)
     {
         if(!isAnimated) return;
@@ -59,6 +81,9 @@ class ExtendedSprite extends FlxSprite
         animation.play(name);
     }
 
+    /**
+     * plays an animation from an specific frame. if the sprite is not animated, this function won't cause effect
+    **/
     public function playFromFrame(name:String, frame:Int)
     {
         if(!isAnimated) return;
@@ -66,11 +91,19 @@ class ExtendedSprite extends FlxSprite
         animation.play(name, false, false, frame);
     }
 
+    /**
+     * shakes an object
+    **/
     public function shakeObject(intensity:Float, time:Float, axes:FlxAxes)
     {
         FlxTween.shake(this, intensity, time, axes);
     }
 
+    /**
+     * if the cursor is overlaping the sprite, it will return `true`
+     *@param pixelPerfect if `true`, the cursor will overlap the sprite if it's touching its pixels and not its hitbox
+     *@param getLastCamera if `true`, the overlap will count from the last camera (useful if you're using several cameras)
+    **/
     public function isOverlaping(pixelPerfect:Bool, getLastCamera:Bool)
     {
         if(pixelPerfect)
@@ -101,6 +134,9 @@ class ExtendedSprite extends FlxSprite
         return false;
     }
 
+    /**
+     * does a tween duh
+    **/
     public function doTween(values:Dynamic, duration:Float, ?options:TweenOptions)
     {
         FlxTween.tween(this, values, duration, options);
